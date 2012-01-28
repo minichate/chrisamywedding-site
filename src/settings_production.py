b6b22662598929a28c8217ac877c9f43d92e4bc0
Christopher <chris@sheepdoginc.ca>
2012-01-28 18:01:39 -0400

    set min height and use cloudfront

--- src/settings_production.py
+++ src/settings_production.py
@@ -9,10 +9,10 @@ AWS_ACCESS_KEY_ID = 'AKIAI3FAOQGNLNXPLP4A'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'chrisamywedding'
 
-STATIC_URL = 'http://assets.chrisamywedding.ca/'
-ADMIN_MEDIA_PREFIX = 'http://assets.chrisamywedding.ca/admin/'
-MEDIA_URL = "http://assets.chrisamywedding.ca/media/"
-CMS_MEDIA_URL = "http://assets.chrisamywedding.ca/cms/"
+STATIC_URL = 'http://cdn.chrisamywedding.ca/'
+ADMIN_MEDIA_PREFIX = 'http://cdn.chrisamywedding.ca/admin/'
+MEDIA_URL = "http://cdn.chrisamywedding.ca/media/"
+CMS_MEDIA_URL = "http://cdn.chrisamywedding.ca/cms/"
 
 AWS_HEADERS = {
     'Cache-Control': 'max-age=86400',
