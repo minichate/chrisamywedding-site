675d7e32fd33d261dfebd3b07e2c02c9edc351f2
Christopher <chris@sheepdoginc.ca>
2012-01-28 17:06:46 -0400

    tweak media settings again

--- src/settings_production.py
+++ src/settings_production.py
@@ -9,9 +9,10 @@ AWS_ACCESS_KEY_ID = 'AKIAI3FAOQGNLNXPLP4A'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'chrisamywedding'
 
-STATIC_URL = 'https://chrisamywedding.s3.amazonaws.com/'
-ADMIN_MEDIA_PREFIX = 'https://chrisamywedding.s3.amazonaws.com/admin/'
-MEDIA_URL = "https://chrisamywedding.s3.amazonaws.com/media/"
+STATIC_URL = 'http://static.chrisamywedding.ca/'
+ADMIN_MEDIA_PREFIX = 'http://static.chrisamywedding.ca/admin/'
+MEDIA_URL = "http://static.chrisamywedding.ca/media/"
+CMS_MEDIA_URL = "http://static.chrisamywedding.ca/cms/"
 
 AWS_HEADERS = {
     'Cache-Control': 'max-age=86400',
