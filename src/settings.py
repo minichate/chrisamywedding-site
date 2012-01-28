135b8b0377fd3203ca3c0772cd04a23f2fd23e68
Christopher <chris@sheepdoginc.ca>
2012-01-28 16:31:42 -0400

    switch storage backends

--- src/settings.py
+++ src/settings.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     'cms.plugins.twitter',
     'django.contrib.admin',
     'gunicorn',
+    'storages',
 )
 
 # A sample logging configuration. The only tangible logging
--- src/settings_production.py
+++ src/settings_production.py
@@ -3,9 +3,12 @@ from settings import *
 DEBUG = False
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
 AWS_ACCESS_KEY_ID = 'AKIAI3FAOQGNLNXPLP4A'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'chrisamywedding'
 
 STATIC_URL = 'http://chrisamywedding.s3.amazonaws.com/'
 ADMIN_MEDIA_PREFIX = 'http://chrisamywedding.s3.amazonaws.com/admin/'
+MEDIA_URL = "http://chrisamywedding.s3.amazonaws.com/media/"
