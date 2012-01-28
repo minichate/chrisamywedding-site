ebe9e8513fd8a4b79f18ed1de0976f658e78b2b6
Christopher <chris@sheepdoginc.ca>
2012-01-28 16:20:21 -0400

    remove caching

--- src/settings_production.py
+++ src/settings_production.py
@@ -2,16 +2,6 @@ from settings import *
 
 DEBUG = False
 
-CACHES = { 
-    'default': { 
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache', 
-        'LOCATION': '{host}:11211'.format(host=os.environ.get('MEMCACHE_SERVERS')), 
-        'username': os.environ.get('MEMCACHE_USERNAME'), 
-        'password': os.environ.get('MEMCACHE_PASSWORD') 
-    } 
-} 
-
-
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = 'AKIAI3FAOQGNLNXPLP4A'
 AWS_SECRET_ACCESS_KEY = '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'
