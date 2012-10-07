3c1cf2c61e54f92dfe4d3063d1262e860151184d
Christopher Troup <chris@sheepdoginc.ca>
2012-10-07 15:40:51 -0300

    use s3 bucket

--- src/settings_production.py
+++ src/settings_production.py
@@ -10,16 +10,16 @@ AWS_ACCESS_KEY_ID = 'AKIAI3FAOQGNLNXPLP4A'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'chrisamywedding'
 
-STATIC_URL = 'http://cdn.chrisamywedding.ca/'
-ADMIN_MEDIA_PREFIX = 'http://cdn.chrisamywedding.ca/admin/'
-MEDIA_URL = "http://cdn.chrisamywedding.ca/media/"
-CMS_MEDIA_URL = "http://cdn.chrisamywedding.ca/cms/"
+STATIC_URL = 'http://chrisamywedding.s3.amazonaws.com/'
+ADMIN_MEDIA_PREFIX = 'http://chrisamywedding.s3.amazonaws.com/admin/'
+MEDIA_URL = "http://chrisamywedding.s3.amazonaws.com/media/"
+CMS_MEDIA_URL = "http://chrisamywedding.s3.amazonaws.com/cms/"
 
-AWS_HEADERS = {
-    'Cache-Control': 'max-age=1',
-}
+#AWS_HEADERS = {
+#    'Cache-Control': 'max-age=1',
+#}
 
 AWS_CALLING_FORMAT = CallingFormat.VANITY
 AWS_QUERYSTRING_AUTH = False
-AWS_S3_CUSTOM_DOMAIN = 'cdn.chrisamywedding.ca'
+AWS_S3_CUSTOM_DOMAIN = 'chrisamywedding.s3.amazonaws.com'
 AWS_S3_SECURE_URLS = False
